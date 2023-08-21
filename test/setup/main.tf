486054b83b66f487b257934fadd222aca2c659e9
Awais Malik <awmalik@google.com>
2023-08-21 11:42:40 -0700

    add artifact registry service

--- test/setup/main.tf
+++ test/setup/main.tf
@@ -27,7 +27,8 @@ module "project" {
   activate_apis = [
     "cloudresourcemanager.googleapis.com",
     "storage-api.googleapis.com",
-    "serviceusage.googleapis.com"
+    "serviceusage.googleapis.com",
+    "artifactregistry.googleapis.com"
   ]
 }
 
