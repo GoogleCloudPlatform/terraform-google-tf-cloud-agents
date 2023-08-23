c0988a6f29b5c678cfe3f4b966ab04ee40294653
Awais Malik <awmalik@google.com>
2023-08-22 17:05:31 -0700

    fixed TestTfcAgentGkeCustom missing time import

--- test/integration/tfc-agent-gke-custom/tfc_agent_gke_custom_test.go
+++ test/integration/tfc-agent-gke-custom/tfc_agent_gke_custom_test.go
@@ -19,6 +19,7 @@ package tfc_agent_gke_custom
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/gcloud"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
@@ -40,10 +41,10 @@ func TestTfcAgentGkeCustom(t *testing.T) {
 		gcloudArgs := gcloud.WithCommonArgs([]string{"--project", projectId, "--region", location, "--format=json"})
 		op := gcloud.Run(t, fmt.Sprintf("container clusters describe %s", clusterName), gcloudArgs)
 
-		assert.Equal(op.Get("status").String(),"RUNNING", "GKE cluster should be up and running")
+		assert.Equal(op.Get("status").String(), "RUNNING", "GKE cluster should be up and running")
 		assert.Equal(op.Get("masterAuth.clusterCaCertificate").String(), clusterCaCertificate, "GKE cluster should be up and running")
 		assert.Equal(op.Get("nodeConfig.serviceAccount").String(), serviceAccount, "GKE cluster should have the right Service Account attached")
-		assert.Equal(op.Get("nodePools.0.status").String(),"RUNNING", "Node pools should have RUNNING status")
+		assert.Equal(op.Get("nodePools.0.status").String(), "RUNNING", "Node pools should have RUNNING status")
 		assert.Equal(op.Get("nodePools.0.config.metadata.cluster_name").String(), clusterName, "Node pools should be attached to the right cluster")
 		assert.Equal(op.Get("nodePools.0.config.serviceAccount").String(), serviceAccount, "Node pools should be attached to the right Service Account attached")
 	})
